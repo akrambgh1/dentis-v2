709ace25a07fc1c1c7a4b7f84a88b519760c0bd1
akrambgh1 <121614726+akrambgh1@users.noreply.github.com>
2025-01-31 22:16:38 +0100

    Revert ".af"
    This reverts commit 3dc14498306947c1bfc16706278d783e3c2ec6c0, reversing
    changes made to 5b2d8145c89a1c70f43bf5095baba12df16f06f3.

--- src/components/Login-page.jsx
+++ src/components/Login-page.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Nav from "./navbar";
@@ -6,64 +5,57 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
 import { toast } from "react-toastify";
 import GoogleButton from "./google";
-import Button from "./Button";
-import { Link } from "react-router-dom";
-
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      toast.success("User logged in successfully", { position: "top-center" });
+      toast.success("user Login successfully", { position: "top-center", });
       setTimeout(() => {
         navigate("/Profile");
       }, 1000);
+      
+      
     } catch (error) {
       console.error("Error signing in with email and password:", error);
-      toast.error(error.message, { position: "top-center" });
+      toast.error(error.message,{position:"top-center",});
     }
   };
 
+ 
+
+  
   return (
-
-
-      <section className="w-screen h-[100dvh] relative flex flex-col items-center justify-center">
-        <Nav></Nav>
-        <form
-          onSubmit={handleLogin}
-          className="w-[80%] h-full flex flex-col justify-center gap-[2rem] p-4 max-[450px]:w-full md:w-[70%] lg:w-[60%] lg:p-8 xl:w-[30%]"
-        >
-          <h1 className="font-bold text-[2rem] text-[#181940]">
-            Hey, welcom back!
-          </h1>
-          <h3 className="text-[#747474]">
-            No account ? <Link className="text-[#181940] cursor-pointer" to="/Register">Register here.</Link>
-          </h3>
-          <input
-            type="text"
-            className="w-full bg-[#eee] px-4 py-2 rounded-[5px] outline-none"
-            placeholder="E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            className="w-full bg-[#eee] px-4 py-2 rounded-[5px] outline-none"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button className="relative h-12 overflow-hidden rounded bg-[#181940] px-5 py-2.5 text-white transition-all duration-300 hover:bg-neutral-800 hover:ring-2 hover:ring-neutral-800 hover:ring-offset-2">
-            <span className="relative">Log In</span>
-          </button>
-
+    <>
+      <section className="w-screen h-screen bg-gray-900">
+        <Nav />
+        <div className="flex flex-col items-center justify-center h-[80%]">
+          <h1 className="text-white text-xl mb-6">Login</h1>
+          <form
+            onSubmit={handleLogin}
+            className="flex flex-col w-[15rem] items-center">
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              className="bg-gray-800 text-white p-2 w-[100%] mb-4"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="bg-gray-800 text-white p-2 w-[100%] mb-4"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="bg-gray-800 text-white p-2">Login</button>
+          </form>
           <GoogleButton />
-        </form>
+        </div>
       </section>
-
+    </>
   );
 }
--- src/components/profile-page.jsx
+++ src/components/profile-page.jsx
@@ -1,10 +1,10 @@
+
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { auth, db } from "./firebase";
 import { getDoc, doc } from "firebase/firestore";
 import { useEffect } from "react";
-import Nav from "./navbar";
 
 export default function Profile() {
   const [userDetails, setUserDetails] = useState(null);
@@ -49,57 +49,56 @@ export default function Profile() {
   }
 
   return (
-    <>
-      <Nav />
-      <section className="w-screen h-auto bg-[#181940] relative flex flex-col items-center justify-center mb-[5rem]">
-        <div className="bg-[#181940] w-full h-[20vh] flex flex-col items-center justify-center">
+    <section className="w-screen h-screen bg-white relative flex items-center justify-center">
+      <div className="bg-[#000000] w-full h-[30vh] absolute top-0"></div>
+
+      <div className="w-[90%] h-[80vh] z-0 pt-[5rem] flex gap-[2rem] p-4 max-lg:flex-col max-lg:items-center max-lg:pt-4 max-md:h-auto">
+        {/* Sidebar */}
+
+        <div className="h-[40vh] max-md:h-auto w-[30%] p-4 flex flex-col gap-[1rem] items-center bg-white rounded-[5px] shadow max-lg:w-full">
           <img
             className="rounded-full h-24 w-24 object-cover"
             src={userDetails?.photo || "/default-photo.png"}
             alt="user"
           />
-        </div>
-
-        <div className="w-full h-full z-0 flex gap-[2rem] max-lg:flex-col max-lg:items-center ">
-          {/* Sidebar */}
-          {/* 
-        <div className="h-[40vh] max-md:h-auto w-[30%] p-4 flex flex-col gap-[1rem] items-center bg-white rounded-[5px] shadow max-lg:w-full">
-          
           <h1 className="text-xl mb-6 max-md:text-[1rem]">
             Welcome, {userDetails?.Firstname || "Guest"}{" "}
             {userDetails?.Lastname || ""}
           </h1>
-        </div> */}
+        </div>
 
-          {/* Main Content */}
+        {/* Main Content */}
 
-          <div className="bg-white w-full max-lg:w-full h-full rounded-t-[5px] flex flex-col">
-            {/* Tabs Navigation */}
-            <div className="w-full p-4 border-b border-gray-300">
-              <div className="flex space-x-4">
-                <button
-                  onClick={() => setActiveTab("tab2")}
-                  className={`p-2 cursor-pointer max-md:text-[1rem] font-[500] ${
-                    activeTab === "tab2"
-                      ? "border-b-2 border-[#181940] text-[#181940]"
-                      : "text-gray-500"
-                  }`}
-                >
-                  Appointments
-                </button>
+        <div className="bg-white w-[70%] max-lg:w-full h-full rounded-[5px] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] flex flex-col">
 
-                <button
-                  onClick={() => setActiveTab("tab1")}
-                  className={`p-2 cursor-pointer max-md:text-[1rem] font-[500] ${
-                    activeTab === "tab1"
-                      ? "border-b-2 border-[#181940] text-[#181940]"
-                      : "text-gray-500"
-                  }`}
-                >
-                  Personal info
-                </button>
+          {/* Tabs Navigation */}
+          <div className="w-full p-4 border-b border-gray-300">
+            <div className="flex space-x-4">
+              
+              <button
+                onClick={() => setActiveTab("tab2")}
+                className={`p-2 cursor-pointer max-md:text-[.75rem] ${
+                  activeTab === "tab2"
+                    ? "border-b-2 border-[#5bc8a8] text-[#5bc8a8]"
+                    : "text-gray-500"
+                }`}
+              >
+                Appointments
+              </button>
+              
+              <button
+                onClick={() => setActiveTab("tab1")}
+                className={`p-2 cursor-pointer max-md:text-[.75rem] ${
+                  activeTab === "tab1"
+                    ? "border-b-2 border-[#5bc8a8] text-[#5bc8a8]"
+                    : "text-gray-500"
+                }`}
+              >
+                Account settings
+              </button>
 
-                {/* <button
+
+              {/* <button
                 onClick={() => setActiveTab("tab3")}
                 className={`p-2 cursor-pointer ${
                   activeTab === "tab3"
@@ -109,82 +108,69 @@ export default function Profile() {
               >
                 Tab 3
               </button> */}
-              </div>
             </div>
+          </div>
 
-            {/* Tabs Content */}
-            <div className="max-md:h-[60vh] w-full py-8 px-8 max-md:p-4">
-              {activeTab === "tab1" && (
-                <div className="grid grid-cols-2 gap-8 h-auto max-md:flex max-md:flex-col">
-                  <div className="flex flex-col gap-2">
-                    <h1 className="max-md:text-[1rem] text-[#181940] font-[500]">
-                      First Name
-                    </h1>
-                    <input
-                      disabled
-                      className="bg-[#f7f7f7] rounded-[5px] text-[#1e1e1e] py-2 px-4 outline-none select-none max-md:text-[.85rem] font-[500]"
-                      value={userDetails?.Firstname || "N/A"}
-                    />
-                  </div>
-
-                  <div className="flex flex-col gap-2">
-                    <h1 className="max-md:text-[1rem] text-[#181940] font-[500]">
-                      Last Name
-                    </h1>
-                    <input
-                      disabled
-                      className="bg-[#f7f7f7] rounded-[5px] text-[#1e1e1e] py-2 px-4 outline-none select-none max-md:text-[.85rem] font-[500]"
-                      value={userDetails?.Lastname || "N/A"}
-                    />
-                  </div>
-
-                  <div className="flex flex-col gap-2">
-                    <h1 className="max-md:text-[1rem] text-[#181940] font-[500]">
-                      Phone Number
-                    </h1>
-                    <input
-                      disabled
-                      className="bg-[#f7f7f7] rounded-[5px] text-[#1e1e1e] py-2 px-4 outline-none select-none max-md:text-[.85rem] font-[500]"
-                      value={userDetails?.Phone || "N/A"}
-                    />
-                  </div>
-
-                  <div className="flex flex-col gap-2">
-                    <h1 className="max-md:text-[1rem] text-[#181940] font-[500]">
-                      E-mail address
-                    </h1>
-                    <input
-                      disabled
-                      className="bg-[#f7f7f7] rounded-[5px] text-[#1e1e1e] py-2 px-4 outline-none select-none max-md:text-[.85rem] font-[500]"
-                      value={userDetails?.email || "N/A"}
-                    />
-                  </div>
-                  <div className="w-full p-8 max-md:pt-4 max-md:p-0 max-md:border-t-1 max-md:border-[#eee]">
-                    <button
-                      onClick={handLogout}
-                      className="relative cursor-pointer overflow-hidden rounded bg-[#181940] px-[2rem] py-2.5 text-white transition-all duration-200 hover:bg-red-500 hover:ring-offset-2 active:ring-2 active:ring-neutral-800"
-                    >
-                      Logout
-                    </button>
-                  </div>
+          {/* Tabs Content */}
+          <div className="border-b-2 border-[#eef0f0] max-md:h-full w-full py-8 px-8 max-md:p-4">
+            {activeTab === "tab1" && (
+              <div className="grid grid-cols-2 gap-8 h-auto max-md:flex max-md:flex-col">
+                <div className="flex flex-col gap-2">
+                  <h1 className="max-md:text-[.75rem]">First Name</h1>
+                  <input
+                    disabled
+                    className="bg-[#fbfdff] rounded-[5px] uppercase py-2 px-4 border-[1px] border-[#ddd] outline-none select-none max-md:text-[.75rem]"
+                    value={userDetails?.Firstname || "N/A"}
+                  />
                 </div>
-              )}
 
-              {activeTab === "tab2" && (
-                <div className="h-auto flex flex-col items-center justify-center">
-                  <p>No appointments available</p>
+                <div className="flex flex-col gap-2">
+                  <h1 className="max-md:text-[.75rem]">Last Name</h1>
+                  <input
+                    disabled
+                    className="bg-[#fbfdff] rounded-[5px] uppercase py-2 px-4 border-[1px] border-[#ddd] outline-none select-none max-md:text-[.75rem]"
+                    value={userDetails?.Lastname || "N/A"}
+                  />
                 </div>
-              )}
 
-              {/* {activeTab === "tab3" && (
+                <div className="flex flex-col gap-2">
+                  <h1 className="max-md:text-[.75rem]">Phone Number</h1>
+                  <input
+                    disabled
+                    className="bg-[#fbfdff] rounded-[5px] uppercase py-2 px-4 border-[1px] border-[#ddd] outline-none select-none max-md:text-[.75rem]"
+                    value={userDetails?.Phone|| "N/A"}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <h1 className="max-md:text-[.75rem]">E-mail address</h1>
+                  <input
+                    disabled
+                    className="bg-[#fbfdff] rounded-[5px] py-2 px-4 border-[1px] border-[#ddd] outline-none select-none max-md:text-[.75rem]"
+                    value={userDetails?.email || "N/A"}
+                  />
+                </div>
+              </div>
+            )}
+
+            {activeTab === "tab2" && (
+              <div className="h-full flex items-center justify-center">
+                <p>No appointments available</p>
+              </div>
+            )}
+
+            {/* {activeTab === "tab3" && (
               <div className="h-full flex items-center justify-center">
                 <p>Content for Tab 3</p>
               </div>
             )} */}
-            </div>
+          </div>
+
+          <div className="w-full p-8 max-md:p-4">
+          <button onClick={handLogout} className="relative cursor-pointer overflow-hidden rounded bg-[#ff2b2b] px-[2rem] py-2.5 text-white transition-all duration-200 hover:bg-red-500 hover:ring-offset-2 active:ring-2 active:ring-neutral-800">Logout</button>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
